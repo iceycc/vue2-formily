642b8c73c1ae60e576b9096f5d59a25163544174
Beyond Wang <wangbingyang@didiglobal.com>
2022-09-02 01:01:58 +0800

    feat: add pebble

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './pebble-ui'
+// import CAPI from '@vue/composition-api'
 Vue.config.productionTip = false
-
+import EleUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.config.productionTip = false;
+// Vue.use(CAPI)
+Vue.use(EleUI)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
